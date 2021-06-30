9afbb9334ca5ed1bca5ddc2556adde13b3ac8f60
Himaloy Mondal <himaloymondal100@gmail.com>
2021-06-30 12:37:10 +0530

    formated

--- Week 1/Lab 1/population/population.c
+++ Week 1/Lab 1/population/population.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+// #include <cs50.h>
 
 int math(int population, int end);
 
@@ -7,11 +8,13 @@ int main(void)
     int start = 0, end = 0;
     do
     {
+        // start = get_int("Start size: ");
         printf("Start size: ");
         scanf("%i", &start);
     } while (start < 9);
     do
     {
+        // end = get_int("End size: ");
         printf("End size: ");
         scanf("%i", &end);
     } while (end < start);
