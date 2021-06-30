35716151cf68a954be87d906c798e2f037d1f1c6
Himaloy Mondal <himaloymondal100@gmail.com>
2021-06-30 12:37:19 +0530

    mario

---
+++ Week 1/mario/mario.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// #include <stido.h>
+int main(void)
+{
+    // Get height
+    int height;
+    do
+    {
+        // height = get_int("Height: ");
+        printf("Height: ");
+        scanf("%i", &height);
+    } while(height < 1 || height > 8);
+
+    // Calculation
+    for(int n = 0; n < height; n++)
+    {
+        for(int x = 1; x < (height - n); x++)
+        {
+            printf(" ");
+        }
+        for(int y = -1; y < n; y++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
