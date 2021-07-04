b49b17f0622400f615e5eb86732fda24bf5af4b8
Himaloy Mondal <himaloymondal100@gmail.com>
2021-07-04 16:17:40 +0530

    scrabble

--- Week 2/Lab 2/scrabble.c
+++ Week 2/Lab 2/scrabble.c
@@ -4,28 +4,29 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
 // Alphabets
 char *alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Prototypes
-// compute_score(string word);
+// int compute_score(string word);
 int compute_score(char word[]);
-int indexof(char character);
 
 int main(void)
 {
     // Initialize variables
     char word1[50];
     char word2[50];
+
     // Get input words from both players
+    // string word1 = get_string("Player 1: ");
     printf("Player 1: ");
     gets(word1);
-    // string word1 = get_string("Player 1: ");
+
+    // string word2 = get_string("Player 2: ");
     printf("Player 2: ");
     gets(word2);
-    // string word2 = get_string("Player 2: ");
     
-
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
@@ -43,13 +44,6 @@ int main(void)
     {
         printf("Tie!\n");
     }
-}
-
-int indexof(char character)
-{
-    char *e;
-    e = strchr(alphabets, character);
-    return (int)(e - alphabets);
 }
 
 // compute_score(string word);
