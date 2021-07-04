ebdbde1e81a0f2c574cb7f22f07aa8bab59dd280
Himaloy Mondal <himaloymondal100@gmail.com>
2021-07-04 16:17:24 +0530

    readability

---
+++ Week 2/readability/readability.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "cs50.c"
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+// Alphabets
+char *alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int main(void)
+{
+    // Initialize variables
+    int words = 1;
+    int sentences = 0;
+    int letters = 0;
+
+    // Get string
+    // string text = get_string("Text: ");
+    string text;
+    printf("Text: ");
+    gets(text);
+
+    // length of the string
+    int n = strlen(text);
+
+    // Count the number of letters
+    for (int i = 0; i < n; i++)
+    {
+        if (strchr(alphabets, toupper(text[i])) != NULL)
+        {
+            letters++;
+        }
+    }
+
+    // Count the number of words
+    for (int i = 0; i < n; i++)
+    {
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+    }
+
+    // Count the number of sentences
+    for (int i = 0; i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+    }
+
+    // Coleman Liau formula
+    float result = 0.0588 * letters / words * 100 - 0.296 * sentences / words * 100 - 15.8;
+
+    // Print result
+    if (result <= 0)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (result >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %d\n", (int)round(result));
+    }
+}
