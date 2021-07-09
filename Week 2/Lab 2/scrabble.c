6e55cfe2ac031e3fdd96945b946fbe7854d3f933
Himaloy Mondal <himaloymondal100@gmail.com>
2021-07-09 13:12:45 +0530

    Removed unwanted lines

--- Week 2/Lab 2/scrabble.c
+++ Week 2/Lab 2/scrabble.c
@@ -5,8 +5,6 @@
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-// Alphabets
-char *alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Prototypes
 int compute_score(string word);
